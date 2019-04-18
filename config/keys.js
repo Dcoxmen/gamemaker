2b1981685dca3ed5113c4a6fcf70a65b83717a83
Dave Cox <davedvst@gmail.com>
2019-04-18 09:59:31 -0700

    updated key

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI: "mongodb://localhost/game_maker",
-  // "mongodb+srv://drcadmin:DcoX11mongo@cluster0-qwq1g.mongodb.net/test?retryWrites=true",
+
   secretOrKey: "secret"
 };
